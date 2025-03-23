d57f392b21fd919804da12601d27f068611c5b1a
chrisvibeofficial <jueffizzy@gmail.com>
2025-03-23 12:49:25 +0100

    donee

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,7 +9,10 @@ exports.createProduct = async (req, res) => {
 
     let image = {};
 
-    if (file) {
+    if (file && file.path) {
+      console.log('file path: ',file.path);
+      console.log('file: ',file);
+      
       const result = await cloudinary.uploader.upload(file.path);
       fs.unlinkSync(file.path);
 
